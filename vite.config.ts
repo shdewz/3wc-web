39ffe8e57d98bf9cc94e0e820a18b98dd568b7ea
shdewz <shdewz@gmail.com>
2025-11-19 16:32:19 +0200

    Switch to HeroUI template

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-import react from "@vitejs/plugin-react";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
