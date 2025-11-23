94a10feca9cc63de35cf76870be72433be3fb0a2
shdewz <shdewz@gmail.com>
2025-11-23 19:40:08 +0200

    Replace footer with external component

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,6 +1,6 @@
-import { Link } from '@heroui/link';
+import { Footer } from '../components/footer.tsx';
 
-import { FullNavbar } from '@/components/navbar';
+import { Navbar } from '@/components/navbar';
 
 export default function DefaultLayout({
   children,
@@ -9,21 +9,11 @@ export default function DefaultLayout({
 }) {
   return (
     <div className="relative flex flex-col h-screen">
-      <FullNavbar />
+      <Navbar />
       <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
         {children}
       </main>
-      <footer className="w-full flex items-center justify-center py-3">
-        <Link
-          isExternal
-          className="flex items-center gap-1 text-current"
-          href="https://heroui.com"
-          title="heroui.com homepage"
-        >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-primary">HeroUI</p>
-        </Link>
-      </footer>
+      <Footer />
     </div>
   );
 }
