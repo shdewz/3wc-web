553d4ea8659f74148c33c2e000e65c02b6d12dc6
shdewz <shdewz@gmail.com>
2025-11-23 19:39:46 +0200

    Adjust site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,69 +1,24 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: 'Vite + HeroUI',
-  description: 'Make beautiful websites regardless of your design experience.',
+  name: '3 Digit World Cup',
+  description: '',
   navItems: [
     {
       label: 'Home',
       href: '/',
     },
     {
-      label: 'Docs',
-      href: '/docs',
+      label: 'Information',
+      href: '/information',
     },
     {
-      label: 'Pricing',
-      href: '/pricing',
+      label: 'Rules',
+      href: '/rules',
     },
     {
-      label: 'Blog',
-      href: '/blog',
-    },
-    {
-      label: 'About',
-      href: '/about',
+      label: 'Schedules',
+      href: '/schedules',
     },
   ],
-  navMenuItems: [
-    {
-      label: 'Profile',
-      href: '/profile',
-    },
-    {
-      label: 'Dashboard',
-      href: '/dashboard',
-    },
-    {
-      label: 'Projects',
-      href: '/projects',
-    },
-    {
-      label: 'Team',
-      href: '/team',
-    },
-    {
-      label: 'Calendar',
-      href: '/calendar',
-    },
-    {
-      label: 'Settings',
-      href: '/settings',
-    },
-    {
-      label: 'Help & Feedback',
-      href: '/help-feedback',
-    },
-    {
-      label: 'Logout',
-      href: '/logout',
-    },
-  ],
-  links: {
-    github: 'https://github.com/heroui-inc/heroui',
-    twitter: 'https://twitter.com/hero_ui',
-    docs: 'https://heroui.com',
-    discord: 'https://discord.gg/9b6yyZKmH4',
-    sponsor: 'https://patreon.com/jrgarciadev',
-  },
 };
