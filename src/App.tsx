21939064f89ab18f2007ccab168f94a822e39911
shdewz <shdewz@gmail.com>
2025-11-23 19:39:28 +0200

    Remove redundant routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { useTheme } from '@heroui/use-theme';
 
 import IndexPage from '@/pages/index';
-import DocsPage from '@/pages/docs';
 import PricingPage from '@/pages/pricing';
-import BlogPage from '@/pages/blog';
-import AboutPage from '@/pages/about';
 
 function App() {
   useTheme('dark');
@@ -13,10 +10,7 @@ function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
       <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
     </Routes>
   );
 }
