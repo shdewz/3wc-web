2c6844ec91fed2411c21fed50d12d9f562bbe248
shdewz <shdewz@gmail.com>
2025-11-24 18:22:13 +0200

    Rename page

--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -1,14 +1,14 @@
 import { title } from '@/components/primitives';
-import DefaultLayout from '@/layouts/default';
+import { DefaultLayout } from '@/layouts/default';
 
-export default function DocsPage() {
+export const InfoPage = () => {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Pricing</h1>
+          <h1 className={title()}>Information</h1>
         </div>
       </section>
     </DefaultLayout>
   );
-}
+};
