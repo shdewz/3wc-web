e245e2f1b444b187eac8c51bfa8f4d05a4e487cf
shdewz <shdewz@gmail.com>
2025-12-21 22:12:29 +0200

    Add proxy for api routes

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,14 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://host.docker.internal:4000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
