96215fe669e2e7e3b05b7d7a5ea3947aae517edf
shdewz <shdewz@gmail.com>
2025-12-21 22:14:11 +0200

    Add user card to navbar menu

--- src/components/navbar/navbar-menu.tsx
+++ src/components/navbar/navbar-menu.tsx
@@ -2,13 +2,47 @@ import {
   Link,
   NavbarMenu as HeroUINavbarMenu,
   NavbarMenuItem,
+  Divider,
+  Card,
+  Button,
+  Avatar,
 } from '@heroui/react';
 
 import { siteConfig } from '@/config/site';
+import { LoginButton } from '@/components/navbar/navbar-login-button';
+import { useAuth } from '@/provider';
+import { LogOutIcon } from '@/components/icons';
 
 export const NavbarMenu = () => {
+  const { user, logout } = useAuth();
+
   return (
     <HeroUINavbarMenu>
+      {user ? (
+        <Card className="p-4 flex flex-row justify-between">
+          <Avatar
+            isBordered
+            showFallback
+            className="transition-transform"
+            color="default"
+            name="Unknown"
+            size="md"
+            src={user?.avatar_url}
+          />
+          <div className="flex text-right items-center gap-3">
+            <div className="flex flex-col">
+              <p className="text-default-600 italic text-xs">Signed in as</p>
+              <p className="font-semibold">{user?.username ?? 'Unknown'}</p>
+            </div>
+            <Button isIconOnly color="danger" variant="flat" onPress={logout}>
+              <LogOutIcon />
+            </Button>
+          </div>
+        </Card>
+      ) : (
+        <LoginButton />
+      )}
+      <Divider />
       {siteConfig.navItems.map((item, index) => (
         <NavbarMenuItem key={`${item.label}-${index}`}>
           <Link
