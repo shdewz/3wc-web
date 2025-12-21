8b9730551372c054b35bfc1fb141632171b65cb8
shdewz <shdewz@gmail.com>
2025-12-21 22:13:25 +0200

    Simplify primitives

--- src/components/primitives.ts
+++ src/components/primitives.ts
@@ -4,12 +4,8 @@ export const title = tv({
   base: 'tracking-tight inline font-semibold',
   variants: {
     color: {
-      violet: 'from-[#FF1CF7] to-[#b249f8]',
-      yellow: 'from-[#FF705B] to-[#FFB457]',
       blue: 'from-[#5EA2EF] to-[#0072F5]',
-      cyan: 'from-[#00b7fa] to-[#01cfea]',
       green: 'from-[#6FEE8D] to-[#17c964]',
-      pink: 'from-[#FF72E1] to-[#F54C7A]',
       foreground: 'dark:from-[#FFFFFF] dark:to-[#4B4B4B]',
     },
     size: {
@@ -26,15 +22,7 @@ export const title = tv({
   },
   compoundVariants: [
     {
-      color: [
-        'violet',
-        'yellow',
-        'blue',
-        'cyan',
-        'green',
-        'pink',
-        'foreground',
-      ],
+      color: ['blue', 'green', 'foreground'],
       class: 'bg-clip-text text-transparent bg-gradient-to-b',
     },
   ],
