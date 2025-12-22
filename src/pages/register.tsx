6a7a8e7bcc8e4069cffd6ec1b6260b58c4e3019b
shdewz <shdewz@gmail.com>
2025-12-22 23:09:38 +0200

    Add base register page

---
+++ src/pages/register.tsx
@@ -0,0 +1,14 @@
+import { title, subtitle } from '@components/primitives';
+
+export const RegisterPage = () => {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 h-full md:py-10">
+      <div className="inline-block max-w-3xl text-center justify-center">
+        <span className={title()}>Register for </span>
+        <span className={title({ color: 'green' })}>3WC&nbsp;2026</span>
+        <br />
+        <div className={subtitle({ class: 'mt-4' })}>Coming January 3</div>
+      </div>
+    </section>
+  );
+};
