306666321b0b0bfb04858b45a56914c2eb45db78
shdewz <shdewz@gmail.com>
2025-12-22 22:39:22 +0200

    Remove unnecessary nbsps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ export const IndexPage = () => {
   return (
     <section className="flex flex-col items-center justify-center gap-4 h-full md:py-10">
       <div className="inline-block max-w-3xl text-center justify-center">
-        <span className={title()}>Get ready for&nbsp;</span>
-        <span className={title({ color: 'green' })}>3WC&nbsp;2026&nbsp;</span>
+        <span className={title()}>Get ready for </span>
+        <span className={title({ color: 'green' })}>3WC&nbsp;2026</span>
         <br />
         <div className={subtitle({ class: 'mt-4' })}>Coming January 3</div>
       </div>
