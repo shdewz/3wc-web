885001271ae5ad79a90ccb8ba0723a2f87407725
shdewz <shdewz@gmail.com>
2025-12-22 23:09:26 +0200

    Add routewithtitle component

---
+++ src/components/common/route-with-title.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+import { siteConfig } from '@/config/site';
+
+interface Props {
+  title?: string;
+  element: React.ReactElement;
+}
+
+export const RouteWithTitle = ({ title, element }: Props) => {
+  useEffect(() => {
+    const base = siteConfig.name;
+
+    document.title = title ? `${title} | ${base}` : base;
+  }, [title]);
+
+  return element;
+};
