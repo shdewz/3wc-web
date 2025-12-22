1dcc8a3ce203a6eb4ae3e13e5f34001a857bafd0
shdewz <shdewz@gmail.com>
2025-12-22 22:38:31 +0200

    Change progress circle to spinner

--- src/components/common/fullscreen-loader.tsx
+++ src/components/common/fullscreen-loader.tsx
@@ -1,7 +1,7 @@
-import { CircularProgress } from '@heroui/react';
+import { Spinner } from '@heroui/react';
 
 export const FullScreenLoader = () => (
   <div className="fixed inset-0 z-50 flex justify-center items-center">
-    <CircularProgress size="lg" />
+    <Spinner size="lg" variant="wave" />
   </div>
 );
