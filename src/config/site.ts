8ee29d0c1786cd591f9cb4b5b5c5c1b02de0ff60
shdewz <shdewz@gmail.com>
2026-01-04 02:22:13 +0200

    Gate nav items

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,16 +1,12 @@
+import { isRegistrationOpen } from '@/utils/registration';
+
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: '3 Digit World Cup',
   description: '',
   navItems: [
-    {
-      label: 'Rules',
-      href: '/rules',
-    },
-    {
-      label: 'Register',
-      href: '/register',
-    },
+    ...(isRegistrationOpen() ? [{ label: 'Rules', href: '/rules' }] : []),
+    ...(isRegistrationOpen() ? [{ label: 'Register', href: '/register' }] : []),
   ],
 };
