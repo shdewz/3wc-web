c904a0dbd8ed4fe838d1a9995903a3d6e3477371
shdewz <shdewz@gmail.com>
2026-01-08 22:21:28 +0200

    Update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,12 +4,12 @@ export const Footer = () => {
   return (
     <footer className="w-full flex flex-col gap-0.5 items-center justify-center py-3 text-xs">
       <div className="flex items-center justify-center gap-3">
-        <Link className="text-xs font-bold" color="foreground" href="/">
+        {/* <Link className="text-xs font-bold" color="foreground" href="/">
           Terms
         </Link>
         <Link className="text-xs font-bold" color="foreground" href="/">
           Privacy Policy
-        </Link>
+        </Link> */}
         <Link
           isExternal
           className="text-xs font-bold"
@@ -20,7 +20,7 @@ export const Footer = () => {
         </Link>
       </div>
       <div className="flex items-center justify-center">
-        <span className="text-default-600">© 2025 shdewz</span>
+        <span className="text-default-600">© 2026 shdewz</span>
       </div>
     </footer>
   );
