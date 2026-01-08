74068af848a6d21c947c2eefc6cd96acdb20754b
shdewz <shdewz@gmail.com>
2026-01-08 21:38:13 +0200

    Remove registration check temporarily

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,12 +1,10 @@
-import { isRegistrationOpen } from '@/utils/registration';
-
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: '3 Digit World Cup',
   description: '',
   navItems: [
-    ...(isRegistrationOpen() ? [{ label: 'Rules', href: '/rules' }] : []),
-    ...(isRegistrationOpen() ? [{ label: 'Register', href: '/register' }] : []),
+    { label: 'Rules', href: '/rules' },
+    { label: 'Register', href: '/register' },
   ],
 };
