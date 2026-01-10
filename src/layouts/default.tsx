cbc345d8992f41504895b7e67912fd3a3f88da25
shdewz <shdewz@gmail.com>
2026-01-11 01:49:41 +0200

    Add missing style prop

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -5,7 +5,7 @@ export const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative flex flex-col h-svh md:h-dvh">
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 grow justify-center items-start pt-16 pb-[env(safe-area-inset-bottom)]">
+      <main className="container mx-auto max-w-7xl px-6 flex grow justify-center items-start pt-16 pb-[env(safe-area-inset-bottom)]">
         {children}
       </main>
       <Footer />
